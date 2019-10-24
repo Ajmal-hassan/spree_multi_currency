51ebaf784746c923d5b4b37fb0083c0a295d4171
Ajmal-hassan <ajmalhassan651@gmail.com>
2019-10-24 20:34:29 +0500

    Fixed issues line_item and product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,13 @@
-module Spree
-  Product.class_eval do
-    alias_method :amount=, :price=
+# module Spree
+#   Product.class_eval do
+#     alias_method :amount=, :price=
+#   end
+# end
+module  Spree
+  module ProductDecorator
+    def self.prepended(base)
+      alias_method :amount=, :price=
+    end
+
   end
 end
